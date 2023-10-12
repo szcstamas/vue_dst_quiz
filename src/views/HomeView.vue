5c7170f3c8fc8657ec29b5e3b684c07c339fd4b9
Tamás Szűcs <x-tamas.szucs2@wizzair.com>
2023-10-12 07:51:12 +0200

    Making separated container for Home component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,49 +1,11 @@
 <template>
-  <section>
-    <div class="home-wrapper">
-      <h1>Szia! Indulhat a játék?</h1>
-      <div class="home-wrapper__button-container">
-        <router-link to="/options">Igen-igen, kezdjük már!</router-link>
-      </div>
-    </div>
-  </section>
+  <home-container />
 </template>
 
 <script>
-export default {};
+import HomeContainer from "../components/home-container/HomeContainer.vue";
+
+export default {
+  components: { HomeContainer },
+};
 </script>
-
-<style lang="scss" scoped>
-.home-wrapper {
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  text-align: center;
-  position: relative;
-
-  &__button-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 2rem;
-    width: 100%;
-    max-width: 1000px;
-    margin: auto;
-
-    a {
-      flex: 1;
-      padding: 2rem;
-      background-color: rgb(3, 94, 0);
-      font-family: "BelisaPlumilla", sans-serif;
-      letter-spacing: 1px;
-      font-size: 54px;
-      max-width: 500px;
-      width: 100%;
-      font-weight: bold;
-      color: #fff;
-      text-decoration: none;
-      border-radius: 10px;
-    }
-  }
-}
-</style>
